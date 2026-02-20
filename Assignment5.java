4067438c50de3ee7657156a27af3c7bd1e0e2991
JordanFrohock1 <jrfro04@gmail.com>
2026-02-19 19:50:53 -0500

    emptyFirstCommit

---
+++ Assignment5.java
@@ -0,0 +1,32 @@
+public class Assignment5 {
+    public static void main(String[] args) {
+       
+    
+
+/*Assignment5.java
+
+git checkout -b add-all-student-names
+git checkout master 
+ - this goes back to the master branch
+git checkout branch add-all-student-names
+ - this goes back to the branch add-all-student-names
+git merge master
+ - this merges the master branch into the add-all-student-names branch
+
+
+
+
+ */
+   /*  System.out.println("Would you like to see a dog or a cat?");
+    String answer = System.console().readLine();
+    if (answer.equals("dog")) {
+        System.out.println("Dog ASCII ART HERE");
+    
+    } else if (answer.equals("cat")) {
+        System.out.println("Cat ASCII ART HERE");
+
+
+    }
+*/
+}
+}
